9085c29aa9ebaf8e340865f535dd5c0628c82cfa
António Nuno Monteiro <anmonteiro@gmail.com>
2014-04-07 18:46:20 +0100

    added scrape function

--- HNScraper.js
+++ HNScraper.js
@@ -1,15 +1,44 @@
+var cheerio = require('cheerio'),
+  request = require('request');
+
 var HNScraper = function() {
-  return {
-    parseArticleElement : function(el, callback){
-      
-      callback(null, {
-        title : 'Learn C and build your own programming language​',
-        url: 'http:​/​/​www.buildyourownlisp.com/​',
-        src: ' (buildyourownlisp.com) ​',
-      });
-    }
-  };
 }
 
+HNScraper.prototype.parseArticleElement = function(el, callback){
+  var err = null;
+  var $ = cheerio.load(el);
+  var result = null;
+  var $a = $('td.title > a');
+  var $span = $('td.title > span');
 
+  if($a.length && $span.length) {
+    result = {
+      title : $a.text(),
+      url : $a.attr('href'),
+      src : $span.text(),
+    };
+  }
+  else {
+  	err = new Error('Invalid element');
+  }
+  
+  callback(err, result);
+}
+
+HNScraper.prototype.scrape = function(callback) {
+  request({
+    uri : 'http://news.ycombinator.com/news',
+    proxy : 'http://extsafira06:extsafira06@10.21.4.37:8080',
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(error, body);
+    }
+    else {
+      error = error || new Error('404');
+      callback(error, null);
+    }
+  });
+}
+
+//'td:not([align]).title'
 module.exports = new HNScraper();
--- test/HNScraper.test.js
+++ test/HNScraper.test.js
@@ -1,25 +1,33 @@
-//var request = require('supertest');
 var chai = require('chai'),
   should = chai.should(),
-  expect = chai.expect;
+  expect = chai.expect,
+  nock = require('nock');
+
 var hn = require('../HNScraper');
 
-describe('HNScraper parse article element function', function(){
+describe('HNScraper parse article element function', function() {
   var el,
+    invalidEl,
     obj;
-  beforeEach(function(){
-    el = '<td class=​"title">​<a href=​"http:​/​/​www.buildyourownlisp.com/​">​'
-      + 'Learn C and build your own programming language​</a>'
-      + '​<span class=​"comhead">​ (buildyourownlisp.com) ​</span>​</td>​';
+  beforeEach(function() {
+    el = '<td class="title"><a href="http://www.buildyourownlisp.com/">' +
+      'Learn C and build your own programming language</a>' +
+      '<span class="comhead"> (buildyourownlisp.com) </span></td>';
     obj = {
-      title : 'Learn C and build your own programming language​',
-      url: 'http:​/​/​www.buildyourownlisp.com/​',
-      src: ' (buildyourownlisp.com) ​',
+      title : 'Learn C and build your own programming language',
+      url: 'http://www.buildyourownlisp.com/',
+      src: ' (buildyourownlisp.com) ',
     };
   });
 
-  it('should parse one DOM element with the article link into an object', function(){
-    hn.parseArticleElement(el, function(err, art){
+  afterEach(function() {
+    el = null;
+    invalidEl = null;
+    obj = null;
+  });
+
+  it('should parse one DOM element with the article link into an object', function() {
+    hn.parseArticleElement(el, function(err, art) {
     	expect(err).to.be.null;
     	expect(err).not.to.be.undefined;
     	art.should.be.ok;
@@ -28,4 +36,59 @@ describe('HNScraper parse article element function', function(){
       art.should.eql(obj);
     });
   });
+
+  it('should return an error when an empty DOM element is passed to the function', function() {
+    hn.parseArticleElement('', function(err, art) {
+      expect(err).not.to.be.null;
+      expect(err).not.to.be.undefined;
+      expect(art).not.to.be.ok;
+    });
+  });
+  
+  it('should also return an error if an invalid element is passed', function() {
+    invalidEl = '<div class="titl"><a href="http://www.buildyourownlisp.com/">' +
+      'Learn C and build your own programming language</a>' +
+      '<span class="comhead"> (buildyourownlisp.com) </span></div>';
+
+      hn.parseArticleElement(invalidEl, function(err, art) {
+        expect(err).not.to.be.null;
+        expect(err).not.to.be.undefined;
+        expect(art).not.to.be.ok;
+      });
+  });
+});
+
+describe('HNScraper.scrape', function() {
+  var api;
+  beforeEach(function() {
+    
+  });
+
+  afterEach(function() {
+    nock.cleanAll();
+  });
+
+  it('should get content when passed the correct url', function(done) {
+    api = nock('http://10.21.4.37:8080')
+      .get('http://news.ycombinator.com/news')
+      .reply(200, 'hi');
+    hn.scrape(function(err, content) {
+      expect(err).to.be.null;
+      expect(content).to.eql('hi');
+      done();
+    });
+  });
+
+  it('should return an error when the server replies 404', function(done) {
+    api = nock('http://10.21.4.37:8080')
+      .get('http://news.ycombinator.com/news')
+      .reply(404);
+
+    hn.scrape(function(err, content) {
+      expect(err).to.be.ok;
+
+      done();
+    });
+  });
+
 });
