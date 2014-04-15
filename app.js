ace5c4f343e0b2dbd06639f954c7e660e396ce29
António Nuno Monteiro <anmonteiro@gmail.com>
2014-04-15 10:55:24 +0100

    update package.json and fix style in HNScraper

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 routes(app);
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function () {
     console.log('Listening on port %d', server.address().port);
 });
 
