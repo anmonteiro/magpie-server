9b5e04aa5c26dfc89a2c787e3346ce9336b71080
António Nuno Monteiro <anmonteiro@gmail.com>
2014-04-20 18:53:32 +0100

    start writing tests for mns

--- test/mns.test.js
+++ test/mns.test.js
@@ -6,6 +6,14 @@ var chai = require('chai'),
 
 var mns = require('../lib/mns');
 
-describe('',function () {
-  it();
+describe('mns with json', function () {
+  var reddit = [ '{"name" : "Reddit","url" : "http://www.reddit.com",',
+    '"type" : "application/json","links" : ["/r/javascript","/r/node"],',
+    '"news_selector" : "data.children",',
+    '"article_selector":{"url":"data.url","src":"data.title","title":"data.domain"}}'
+    ].join('');
+
+  it('should read the content type is JSON and handle it as JSON', function (done) {
+    
+  });
 });
