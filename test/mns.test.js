678c105fab183ba128d28d6c6dc753e175a1a444
António Nuno Monteiro <anmonteiro@gmail.com>
2014-04-23 19:25:45 +0100

    some more tests

--- test/mns.test.js
+++ test/mns.test.js
@@ -5,30 +5,47 @@ var chai = require('chai'),
   cheerio = require('cheerio');
 
 var mns = require('../lib/mns');
-
-describe('the same instance of mns', function() {
-  var reddit = [
-    '{"name" : "Reddit","url" : "http://www.reddit.com",',
-    '"type" : "application/json","links" : ["/r/javascript/.json","/r/node/.json"],',
-    '"news_selector" : "data.children",',
-    '"article_selector":{"url":"data.url","src":"data.title","title":"data.domain"}}'
+var reddit = [
+  '{"name" : "Reddit","url" : "http://www.reddit.com",',
+  '"type" : "application/json","links" : ["/r/javascript/.json","/r/node/.json"],',
+  '"news_selector" : "data.children",',
+  '"article_selector":{"url":"data.url","src":"data.title","title":"data.domain"}}'
+].join(''),
+  echojs = [
+    '{"name" : "EchoJS", "url" : "http://www.echojs.com", "type" : ',
+    '"text/html", "links" : [ "/r/javascript", "/r/node" ], "news_selector" : ""}'
   ].join(''),
-    echojs = [
-      '{"name" : "EchoJS", "url" : "http://www.echojs.com", "type" : ',
-      '"text/html", "links" : [ "/r/javascript", "/r/node" ], "news_selector" : ""}'
-    ].join(''),
-    invalid_echojs = [
-      '{"name" : "EchoJS","url" : "http://www.echojs.com",',
-      '"links" : [ "/r/javascript", "/r/node" ], "news_selector" : ""}'
-    ].join('');
+  invalid_echojs = [
+    '{"name" : "EchoJS","url" : "http://www.echojs.com",',
+    '"links" : [ "/r/javascript", "/r/node" ], "news_selector" : ""}'
+  ].join('');
 
-  var scraper;
+var scraper;
+
+describe('calling mns without new operator', function() {
 
   afterEach(function() {
     scraper = null;
   });
 
+  it('should return an instance of mns', function() {
+    scraper = mns(echojs);
+    expect(scraper).to.be.ok;
+    expect(scraper).to.be.instanceOf(mns);
+  });
 
+  it('should not pollute the global scope', function() {
+    scraper = mns(reddit);
+    expect(global).to.exist;
+    expect(global).not.to.contain.keys('items','type');
+  });
+});
+
+describe('the same instance of mns', function() {
+
+  afterEach(function() {
+    scraper = null;
+  });
 
   reddit = JSON.parse( reddit );
   echojs = JSON.parse( echojs );
@@ -47,12 +64,24 @@ describe('the same instance of mns', function() {
 
   it('should throw an error when invalid configuration is passed', function() {
     expect(function() {
-      scraper = mns(false);
+      scraper = mns(true);
     }).to.throw();
 
     expect(function() {
-      
-    });
+      scraper = mns('not an object');
+    }).to.throw();
+
+  });
+});
+
+describe('an mns instance', function() {
+  afterEach(function() {
+    scraper = null;
+  });
+
+  it('should have mandatory properties', function() {
+    scraper = mns(reddit);
+    expect(scraper).to.contain.keys('type', 'url', 'selector'); //pensar nome disto
   });
 });
 
