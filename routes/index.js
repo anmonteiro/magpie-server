0550df1c8d207b002eda316afb2720dce9f4137d
António Nuno Monteiro <anmonteiro@gmail.com>
2014-05-03 22:29:05 +0100

    allow CORS

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var sites = require( './sites' );
 
 function setup( app ) {
+  
+  app.all( '*', function(req, res, next) {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    res.header( 'Access-Control-Allow-Methods', 'GET' );
+    res.header( "Access-Control-Allow-Headers", "X-Requested-With" );
+    next();
+  });
 
   app.get( '/sites/:id', sites.getSite );
   //app.get( '/sites/*', sites.handle )
