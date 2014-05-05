c43b281d4fc32b65a9711dd555dea72e92a735ea
António Nuno Monteiro <anmonteiro@gmail.com>
2014-05-05 23:18:52 +0100

    make port expect process.env.PORT

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = express();
 
 routes.setup(app);
 
-var server = app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+var server = app.listen( port, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
